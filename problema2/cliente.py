943904c152c03de01087d0cef04586476e51656b
Kevin D Preciado <kdpreciado@sitio.com>
2026-02-16 19:32:28 -0500

    solucion Problema 1 y 2 sin todo

--- problema2/cliente.py
+++ problema2/cliente.py
@@ -6,30 +6,30 @@ Objetivo: Crear un cliente TCP que envíe un mensaje al servidor y reciba la mis
 
 import socket
 
-# TODO: Definir la dirección y puerto del servidor
+# : Definir la dirección y puerto del servidor
 HOST ="localhost"
 PORT = 9000
 # Solicitar mensaje al usuario por consola
 mensaje = input("Mensaje: ")
 
-# TODO: Crear un socket TCP/IP
+# : Crear un socket TCP/IP
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # AF_INET: socket de familia IPv4
 # SOCK_STREAM: socket de tipo TCP (orientado a conexión)
 
-# TODO: Conectar el socket al servidor en la dirección y puerto especificados
+# : Conectar el socket al servidor en la dirección y puerto especificados
 cliente.connect((HOST, PORT))
 # Mostrar mensaje que se va a enviar
 print(f"Mensaje '{mensaje}' enviado.")
 
-# TODO: Codificar el mensaje a bytes y enviarlo al servidor
+# : Codificar el mensaje a bytes y enviarlo al servidor
 # sendall() asegura que todos los datos sean enviados
 cliente.sendall(mensaje.encode())
 print(f"Mensaje enviado: '{mensaje}'")
-# TODO: Recibir datos del servidor (hasta 1024 bytes)
+# : Recibir datos del servidor (hasta 1024 bytes)
 respuesta = cliente.recv(1024)
 # Decodificar e imprimir los datos recibidos
 print(f"Respuesta del 'Echo': , '{respuesta.decode()}'")
 
-# TODO: Cerrar la conexión con el servidor
+# : Cerrar la conexión con el servidor
 cliente.close()
